72b82b90a0e5a56717511544162ddb9453c5a01d
boroda4436@gmail.com <zamanalumene5>
2016-10-29 17:22:53 +0300

    displaying numbers by click on btn

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-$('.buttonNumber').click(function() {
-	console.log('succes')
+$(document).ready(function() {
+	$('.buttonNumber').click(function() {
+		$('#monitor').append($(this).val());	
+	});
+	
+	$('#cleanAllBtn').click(function(){
+		$('#monitor').empty();
+	});
+	
+	$('#division, #multiplication, #subtraction, #summary').click(function() {
+		$('#monitor').append($(this).text());	
+	});
+
 })
+/*
+var monitor = 125 + (36+10*(25 + (3+2)*5));
+function readString(str){
+	if(str.contains('(')){
+		var index1 = str.getFirstIndex(str);
+		var index2 = str.getLastIndex(str);
+		var newStr = str.substring(index1, index2);
+		if(newStr.length>0){
+			readString(newStr);
+		}
+	} else{
+		return calculate(str);
+	}
+}
+*/
